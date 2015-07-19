a7e9c0711a84c54d159d0e26c183af5f88819aae
Shinmera <shinmera@tymoon.eu>
2015-07-19 19:43:09 +0200

    Some colouring / label fixes

--- status.py
+++ status.py
@@ -2,36 +2,41 @@ from i3pystatus import Status
 
 status = Status(standalone=True)
 
+def format_label(label, text):
+    return "<span color=\"#FFDD00\">"+label+"</span> "+text
+
 status.register("text", text="")
 
-status.register("clock", format="%a %-d %b %X")
+status.register("clock",
+                format=format_label("DATE","%a %-d %b %X"),
+                hints={"markup":"pango"})
 
 status.register("network",
                 interface="wlp4s0",
-                format_up="NET {v4}",
+                format_up=format_label("NET","{v4}"),
                 format_down="NET DOWN",
                 color_up="#FFFFFF",
-                color_down="#FF0000")
+                color_down="#FF0000",
+                hints={"markup":"pango"})
 
 status.register("battery",
-                format="BAT {percentage:.2f}%{status} {remaining:%E%hh:%Mm} {consumption:.2f}W",
+                format=format_label("BAT","{percentage:.2f}%{status} {remaining:%E%hh:%Mm} {consumption:.2f}W"),
                 alert=True,
                 alert_percentage=5,
-                status={
-                    "DIS": "↓",
-                    "CHR": "↑",
-                    "FULL": "=",
-                })
+                status={"DIS":"↓", "CHR":"↑", "FULL":"="},
+                hints={"markup":"pango"})
 
 status.register("mem",
-                format="MEM {percent_used_mem:02}%",
+                format=format_label("MEM","{percent_used_mem:02}%"),
                 warn_percentage=75,
                 alert_percentage=90,
                 color="#FFFFFF",
                 warn_color="#FFFF00",
-                alert_color="#FF0000")
+                alert_color="#FF0000",
+                hints={"markup":"pango"})
 
 status.register("cpu_usage",
-                format="CPU {usage:02}%")
+                format=format_label("CPU","{usage:02}%"),
+                hints={"markup":"pango"})
 
 status.run()
