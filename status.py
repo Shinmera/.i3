6f43cf1389f32a7201c7dd4c9e44b9fa2f4e11aa
Shinmera <shinmera@tymoon.eu>
2015-08-01 16:50:20 +0200

    Updated status.py to work in other setups without battery/ethernet.

--- status.py
+++ status.py
@@ -1,4 +1,6 @@
 from i3pystatus import Status
+import os
+import netifaces
 
 status = Status(standalone=True)
 
@@ -11,20 +13,23 @@ status.register("clock",
                 format=format_label("DATE","%a %-d %b %X"),
                 hints={"markup":"pango"})
 
-status.register("network",
-                interface="wlp4s0",
-                format_up=format_label("NET","{v4}"),
-                format_down="NET DOWN",
-                color_up="#FFFFFF",
-                color_down="#FF0000",
-                hints={"markup":"pango"})
+for interface in ['wlp4s0','enp7s0']:
+    if interface in netifaces.interfaces():
+        status.register("network",
+                        interface=interface,
+                        format_up=format_label("NET","{v4}"),
+                        format_down="NET DOWN",
+                        color_up="#FFFFFF",
+                        color_down="#FF0000",
+                        hints={"markup":"pango"})
 
-status.register("battery",
-                format=format_label("BAT","{percentage:.2f}%{status} {remaining:%E%hh:%Mm} {consumption:.2f}W"),
-                alert=True,
-                alert_percentage=5,
-                status={"DIS":"↓", "CHR":"↑", "FULL":"="},
-                hints={"markup":"pango"})
+if os.listdir("/sys/class/power_supply/") != []: 
+    status.register("battery",
+                    format=format_label("BAT","{percentage:.2f}%{status} {remaining:%E%hh:%Mm} {consumption:.2f}W"),
+                    alert=True,
+                    alert_percentage=5,
+                    status={"DIS":"↓", "CHR":"↑", "FULL":"="},
+                    hints={"markup":"pango"})
 
 status.register("mem",
                 format=format_label("MEM","{percent_used_mem:02}%"),
